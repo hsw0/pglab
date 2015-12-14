456dee4a6a98871df7bc8d39968a9a55ad6062c1
hsw <hsw@syscall.io>
2015-12-14 15:04:10 +0900

    provision: fix initdb.sh errors

--- provision/scripts/initdb.sh
+++ provision/scripts/initdb.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-PGDATA=/srv/postgresql/data
+PGDATA=/data/pg_data
 
-if [[ -d "$PGDATA" ]]; then
+if [[ -d "$PGDATA/global" ]]; then
 	echo "Database already created."
 	exit 0
 fi
 
+# 데이터 디렉터리 생성
+sudo install -d -o postgres -g postgres -m 700 "$PGDATA"
+
+# DB 생성
 sudo -u postgres /usr/pgsql-9.3/bin/initdb \
 	--encoding=UTF-8 \
 	--locale=en_US.UTF-8 \
